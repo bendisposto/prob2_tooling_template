06af10b84ffed7cac315c29945ddc14bda8c54a8
Jens Bendisposto <jens@bendisposto.de>
2014-07-11 14:05:19 +0200

    Example class + run build task

---
+++ src/main/java/HelloProB.java
@@ -0,0 +1,27 @@
+import de.prob.Main;
+import de.prob.scripting.Api;
+
+public class HelloProB {
+
+  private static Thread prob;
+
+	public static void runProB(final Main m, final String[] args) {
+		prob = new Thread(new Runnable() {
+			public void run() {
+				m.main(args);
+			}
+		});
+    prob.start();
+
+	}
+
+	public static void main(String[] args) {
+
+		Main m = Main.getInjector().getInstance(Main.class);
+		runProB(m, args);
+
+		Api api = Main.getInjector().getInstance(Api.class);
+		System.out.println("\n\n ####  ProB Binary Version: "+api.getVersion()+"\n\n");
+    System.exit(0);
+	}
+}
